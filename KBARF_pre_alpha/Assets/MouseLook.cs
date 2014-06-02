9fc2f108e8596bc585ced58b56d0e33bdc26e591
Milton Plotkin <plotkinmilton@gmail.com>
2014-06-02 18:18:56 +1000

    Spacey screen rotation + Hold space to stop

--- KBARF_pre_alpha/Assets/MouseLook.cs
+++ KBARF_pre_alpha/Assets/MouseLook.cs
@@ -9,8 +9,14 @@ public class MouseLook : MonoBehaviour {
 	private const float GAME_SCREEN_LOOK_SPEED 	   = 0.08f;
 
 	[SerializeField] private float lookSpeed = 1.0f;	// Speed at which the mouse aim moves.
+
 	[SerializeField] private float moveSpeed = 0.1f;	// Maximum speed at which the player moves.
-	[SerializeField] private float accelSpeed = 0.02f;	// Speed at which the player gains speed.
+	[SerializeField] private float moveAccel = 0.02f;	// Speed at which the player gains speed.
+	[SerializeField] private float rotSpeed = 1.0f;		// Maximum speed at which the player rotates their view.
+	[SerializeField] private float rotAccel = 0.02f;	// Speed at which the player accelerates their rotation.			
+	private Vector3 			   move = Vector3.zero;	// Camera's position
+	private float 				   rot = 0.0f;			// Camera's rotation.
+
 	[SerializeField] private float friction = 0.92f;	// Speed at which the speed is worn down.
 
 	private float xAim 		= 0.0f;						// The aim of the mouselook.
@@ -21,8 +27,7 @@ public class MouseLook : MonoBehaviour {
 
 	private GameScreen gameScreen = null;				// The GameScreen the player is currently assigned to.	
 
-	private Vector3 camRotation = Vector3.zero;
-	private Vector3 camMove = Vector3.zero;
+
 
 	void Start() {
 
@@ -44,24 +49,32 @@ public class MouseLook : MonoBehaviour {
 
 	private void Move()
 	{
-		camMove *= friction;
+		float frictionExtra = 0.0f;
+		if (Input.GetKey("space")) frictionExtra = 0.02f;
 
-		if 		(Input.GetKey("w")) camMove += transform.forward*accelSpeed;
-		else if (Input.GetKey("s")) camMove -= transform.forward*accelSpeed;
+		move *= friction - frictionExtra;
+		rot *= friction - frictionExtra;
+
+		if 		(Input.GetKey("w")) move += transform.forward*moveAccel;
+		else if (Input.GetKey("s")) move -= transform.forward*moveAccel;
 		
-		if 		(Input.GetKey("a"))	camMove -= transform.right*accelSpeed;
-		else if (Input.GetKey("d")) camMove += transform.right*accelSpeed;
+		if 		(Input.GetKey("a"))	move -= transform.right*moveAccel;
+		else if (Input.GetKey("d")) move += transform.right*moveAccel;
 
-		if 		(Input.GetKey("q")) transform.eulerAngles += new Vector3(0.0f, 0.0f, 1.0f);
-		else if (Input.GetKey("e")) transform.eulerAngles -= new Vector3(0.0f, 0.0f, 1.0f);
+		if 		(Input.GetKey("q")) rot += rotAccel;
+		else if (Input.GetKey("e")) rot -= rotAccel;
 
-		if (camMove.magnitude > moveSpeed)
+		if (move.magnitude > moveSpeed)
 		{
-			camMove.Normalize();
-			camMove *= moveSpeed;
+			move.Normalize();
+			move *= moveSpeed;
 		}
 
-		transform.position += camMove;
+		if 		(rot > rotSpeed) 	rot = rotSpeed;
+		else if (rot < -rotSpeed) 	rot = -rotSpeed;
+
+		transform.position += move;
+		transform.eulerAngles += new Vector3(0.0f, 0.0f, rot);
 	}
 
 	public void EnterGameScreen(GameScreen gs)
@@ -89,7 +102,7 @@ public class MouseLook : MonoBehaviour {
 		{
 			gameScreen = null;
 			state = "";
-			camMove = Vector3.zero;
+
 			return;
 		}
 
@@ -133,6 +146,11 @@ public class MouseLook : MonoBehaviour {
 		// If the player is looking at the screen, move them to the ideal position.
 		if (state == "screen_lock")
 		{
+			// Stop any velocity you had.
+			move = Vector3.zero;
+			rot = 0.0f;
+
+			// Move towards the screen
 			transform.position 		= Stat3DMove.MoveToPos(transform.position, 		gameScreen.targetPos, GAME_SCREEN_LOOK_SPEED);
 			transform.eulerAngles 	= Stat3DMove.MoveToRot(transform.eulerAngles, 	gameScreen.targetRot, GAME_SCREEN_LOOK_SPEED);
 
