53ffe8e5e47ca1fbe09f8541a776ae39a4570aa5
Milton Plotkin <plotkinmilton@gmail.com>
2014-06-24 19:09:30 +1000

    Pass

--- KBARF_pre_alpha/Assets/Minigames/EditModeSnap.cs
+++ KBARF_pre_alpha/Assets/Minigames/EditModeSnap.cs
@@ -4,20 +4,22 @@ using System.Collections;
 
 [ExecuteInEditMode]
 public class EditModeSnap : MonoBehaviour {
-	public float snapValue = 0.08f;
+	public float snapValueX = 0.08f;
+	public float snapValueY = 0.08f;
 	public float depth = 0.0f;
 
 	void Update() {
 
 		#if (UNITY_EDITOR)
-		float snapInverse = 1/snapValue;
+		float snapInverseX = 1/snapValueX;
+		float snapInverseY = 1/snapValueY;
 		
 		float x, y, z;
 		
 		// if snapValue = .5, x = 1.45 -> snapInverse = 2 -> x*2 => 2.90 -> round 2.90 => 3 -> 3/2 => 1.5
 		// so 1.45 to nearest .5 is 1.5
-		x = Mathf.Round(transform.position.x * snapInverse)/snapInverse;
-		y = Mathf.Round(transform.position.y * snapInverse)/snapInverse;
+		x = Mathf.Round(transform.position.x * snapInverseX)/snapInverseX;
+		y = Mathf.Round(transform.position.y * snapInverseY)/snapInverseY;
 		z = depth; // depth from camera
 		
 		transform.position = new Vector3(x, y, z);
--- KBARF_pre_alpha/Assets/Minigames/PlatformGame/Scripts/PlatCamera.cs
+++ KBARF_pre_alpha/Assets/Minigames/PlatformGame/Scripts/PlatCamera.cs
@@ -12,10 +12,10 @@ public class PlatCamera : MonoBehaviour {
 	void Update () {
 		GameObject hero = GameObject.Find ("pre_hero");
 
-		float x = Mathf.Floor((hero.transform.position.x+1.28f) / 2.56f);
-		float y = Mathf.Floor((hero.transform.position.y+0.96f) / 1.92f);
+		float x = Mathf.Floor((hero.transform.position.x) / 2.56f);
+		float y = Mathf.Floor((hero.transform.position.y) / 1.92f);
 
 		// Move the camera based on hero position.
-		transform.position = new Vector3 (x*2.56f, y*1.92f, -5.0f);
+		transform.position = new Vector3 (x*2.56f+1.28f, y*1.92f+0.96f, -5.0f);
 	}
 }
