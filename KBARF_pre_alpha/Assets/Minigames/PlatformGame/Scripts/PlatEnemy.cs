af11fe652ac447d16690c38d92d9241d1b597b39
Milton Plotkin <plotkinmilton@gmail.com>
2014-06-24 21:53:52 +1000

    Added basic 2D enemy

---
+++ KBARF_pre_alpha/Assets/Minigames/PlatformGame/Scripts/PlatEnemy.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatEnemy : MonoBehaviour {
+
+	MiniCommon mc;
+	PlatCollisions pc;
+
+	[SerializeField] private float animSpeed = 0.1f;
+
+	void Awake ()
+	{
+		mc = GetComponent<MiniCommon> ();	
+		pc = GetComponent<PlatCollisions> ();	
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "PlatHero")
+			print ("Whoops");
+		
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+		GetComponent<Animator>().speed = animSpeed;
+
+		mc.XSpeed = mc.YSpeed = 0.2f;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
