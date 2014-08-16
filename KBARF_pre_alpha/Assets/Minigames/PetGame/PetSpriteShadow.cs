c1a7dae12d89bf8544c2ab4c12fe31cc1af82059
Milton Plotkin <plotkinmilton@gmail.com>
2014-08-16 17:16:40 +1000

    PetGame begins!

---
+++ KBARF_pre_alpha/Assets/Minigames/PetGame/PetSpriteShadow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PetSpriteShadow : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		renderer.castShadows = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ KBARF_pre_alpha/Assets/Minigames/PetGame/PetWireframe.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PetWireframe : MonoBehaviour {
+
+	void Create() {
+		OnPreRender ();
+	}
+
+	void OnPreRender() {
+		GL.Color(Color.yellow);
+		GL.wireframe = true;
+	}
+
+	void OnPostRender() {
+		GL.wireframe = false;
+	}
+}
