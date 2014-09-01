3fa4d6eb6c103bf289cdb53028d7f84b6e88f91b
Milton Plotkin <plotkinmilton@gmail.com>
2014-09-01 17:52:31 +1000

    pass

---
+++ KBARF_pre_alpha/Assets/Minigames/BatGame/BatHUD.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vectrosity;
+
+public class BatHUD : MonoBehaviour {
+
+	[SerializeField] private Vector3 	barPos;
+	[SerializeField] private float	 	width = 100.0f;
+	[SerializeField] private Material	mat;
+
+	private VectorLine[] barEnergy;
+
+	// Use this for initialization
+	void Start () {
+
+		barEnergy = new VectorLine[9];
+
+		for (int i = 0; i < barEnergy.Length; i++)
+		{
+			barEnergy[i] = VectorLine.SetLine3D (Color.white,
+			                          barPos + Vector3.down*(float)i*2.4f,
+			                          barPos + Vector3.right * width + Vector3.down*(float)i*2.4f);
+
+			barEnergy[i].lineWidth = 30.0f;
+			barEnergy[i].material = mat;
+			barEnergy[i].endCap = "Cap";
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
