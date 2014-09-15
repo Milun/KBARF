6771c9f8e50c09decadee67fc9d67dcbbfa104aa
Milton Plotkin <plotkinmilton@gmail.com>
2014-09-16 08:53:13 +1000

    Pass

---
+++ KBARF_pre_alpha/Assets/Minigames/PlatformGame/Scripts/PlatPhysStairs.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(TwoColPhysics))]
+public class PlatPhysStairs : MonoBehaviour {
+
+	private TwoCommon 		pCommon;
+	private TwoColLine 		tColLine;
+
+	private float anchorLength = 8.0f;
+
+	void Awake ()
+	{
+		pCommon 	= GetComponent<TwoCommon> ();
+		tColLine 	= GetComponent<TwoColLine> ();
+	}
+
+	void Update()
+	{
+		tColLine.ColManager.CheckColMove (tColLine, TwoCol.ColType.T3_DEF);
+	}
+}
