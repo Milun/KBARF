5ef929325a28acebe630a32701175ea0f0511fce
Milton Plotkin <plotkinmilton@gmail.com>
2014-09-23 00:39:44 +1000

    Kangaroo (is buggy too)

---
+++ KBARF_pre_alpha/Assets/Minigames/PlatformGame/Scripts/PlatEnemyHop.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatEnemyHop : MonoBehaviour {
+
+	private TwoCommon 		pCommon;
+
+	[SerializeField] private float 		jumpHeight = 2.0f;
+	[SerializeField] private float 		fallSpeed = 0.05f;
+
+	void Awake ()
+	{
+		pCommon 	= GetComponent<TwoCommon> ();
+		//GetComponent<Animator>().
+	}
+
+	void Update()
+	{
+		pCommon.YSpeed -= fallSpeed;
+		if (pCommon.YSpeed < -2.0f) pCommon.YSpeed = -2.0f;
+	}
+}
