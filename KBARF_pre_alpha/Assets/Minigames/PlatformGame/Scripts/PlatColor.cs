c4e8694309bc9af525220f8276f9574c83aca737
Milton Plotkin <plotkinmilton@gmail.com>
2015-01-06 20:36:48 +1100

    WIP: PlatColor

--- KBARF_pre_alpha/Assets/Minigames/PlatformGame/Scripts/PlatColor.cs
+++ KBARF_pre_alpha/Assets/Minigames/PlatformGame/Scripts/PlatColor.cs
@@ -35,51 +35,51 @@ public class PlatColor : MonoBehaviour {
 		switch (spriteColor)
 		{
 		case SpriteColor.WHITE:
-			spriteRenderer.color = new Color(255f/1f, 255f/1f, 255f/1f);
+			spriteRenderer.color = new Color32(255, 255, 255, 255);
 			break;
 
 		case SpriteColor.RED:
-			spriteRenderer.color = new Color(136f/1f, 57f/1f, 50f/1f);
+			spriteRenderer.color = new Color32(136, 57, 50, 255);
 			break;
 
 		case SpriteColor.PINK:
-			spriteRenderer.color = new Color(184f/1f, 105f/1f, 98f/1f);
+			spriteRenderer.color = new Color32(184, 105, 98, 255);
 			break;
 
 		case SpriteColor.ORANGE:
-			spriteRenderer.color = new Color(139f/1f, 84f/1f, 41f/1f);
+			spriteRenderer.color = new Color32(139, 84, 41, 255);
 			break;
 
 		case SpriteColor.YELLOW:
-			spriteRenderer.color = new Color(191f/1f, 206f/1f, 114f/1f);
+			spriteRenderer.color = new Color32(191, 206, 114, 255);
 			break;
 
 		case SpriteColor.GREEN_LIGHT:
-			spriteRenderer.color = new Color(148f/1f, 224f/1f, 137f/1f);
+			spriteRenderer.color = new Color32(148, 224, 137, 255);
 			break;
 
 		case SpriteColor.GREEN_DARK:
-			spriteRenderer.color = new Color(85f/1f, 160f/1f, 73f/1f);
+			spriteRenderer.color = new Color32(85, 160, 73, 255);
 			break;
 
 		case SpriteColor.BLUE:
-			spriteRenderer.color = new Color(103f/1f, 182f/1f, 189f/1f);
+			spriteRenderer.color = new Color32(103, 182, 189, 255);
 			break;
 
 		case SpriteColor.PURPLE_LIGHT:
-			spriteRenderer.color = new Color(120f/1f, 105f/1f, 196f/1f);
+			spriteRenderer.color = new Color32(120, 105, 196, 255);
 			break;
 
 		case SpriteColor.PURPLE_DARK:
-			spriteRenderer.color = new Color(64f, 49f, 141f);
+			spriteRenderer.color = new Color32(64, 49, 141, 255);
 			break;
 
 		case SpriteColor.MAGENTA:
-			spriteRenderer.color = new Color(139f, 63f, 150f);
+			spriteRenderer.color = new Color32(139, 63, 150, 255);
 			break;
 
 		case SpriteColor.BROWN:
-			spriteRenderer.color = new Color(87f, 66f, 0f);
+			spriteRenderer.color = new Color32(87, 66, 0, 255);
 			break;
 
 		default:
